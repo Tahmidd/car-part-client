4cfe8e48a4f8069ead1a41a5b32641f2ebe7de62
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-24 01:16:43 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className='max-w-7xl mx-auto px-12'>
+      <Navbar></Navbar>
 
     </div>
   );
