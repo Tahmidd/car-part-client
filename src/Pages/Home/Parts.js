a4a9da926f70982d9bf8a03a2ad6b37dd11d7436
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-24 02:46:52 +0600

    business summary added

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -11,7 +11,7 @@ const Parts = () => {
     return (
         <div className='my-10'>
             <h2 className='text-3xl font-bold text-center text-primary my-5'>Parts Available</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     parts.slice(0, 3).map(part => <Part
                         key={part.id}
